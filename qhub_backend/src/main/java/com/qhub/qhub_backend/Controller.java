6b578e224e258c1015c435203aa22008ee65f33f
Marvellous <itzmhizterlouis@gmail.com>
2024-05-11 15:12:40 +0100

    Some minor fixes

--- qhub_backend/src/main/java/com/qhub/qhub_backend/Controller.java
+++ qhub_backend/src/main/java/com/qhub/qhub_backend/Controller.java
@@ -1,5 +1,7 @@
 package com.qhub.qhub_backend;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,26 +11,14 @@ import java.time.temporal.ChronoUnit;
 
 @RestController
 @RequestMapping("api/v1/")
+@AllArgsConstructor
 public class Controller {
+
+    private final Service service;
 
     @GetMapping("year-percentage")
     public YearPercentageResponse getYearPercentage() {
 
-        LocalDate currentDate = LocalDate.now();
-
-        LocalDate startOfYear = LocalDate.of(currentDate.getYear(), 1, 1);
-        LocalDate endOfYear = LocalDate.of(currentDate.getYear(), 12, 31);
-
-        long daysPassed = ChronoUnit.DAYS.between(startOfYear, currentDate);
-
-        long totalDaysInYear = ChronoUnit.DAYS.between(startOfYear, endOfYear) + 1;
-
-        double percentageLeft = (1 - (double) daysPassed / totalDaysInYear) * 100;
-
-        String percentage = String.format("%.2f", percentageLeft);
-
-        return YearPercentageResponse.builder()
-                .percent(percentage)
-                .build();
+        return service.getYearPercentage();
     }
 }
